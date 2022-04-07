24e93aaf2f7b0559436474d7e55f36a3cff29e9b
JuanDQuintero <juan.quinterou@udea.edu.co>
2022-04-06 21:16:19 -0500

    frontendv1

---
+++ src/layouts/Navigation/navList.js
@@ -0,0 +1,77 @@
+import {
+  DashboardOutlined,
+  Person,
+  FolderOpen,
+  BorderColor,
+  CalendarMonth,
+  Group
+} from "@mui/icons-material";
+
+const navBarList = [
+  {
+    icon: CalendarMonth,
+    desc: "Calendario",
+    secondDesc: "",
+    subList: [],
+    link: "/calendario"
+  },
+  {
+    icon: FolderOpen,
+    desc: "Matricula",
+    secondDesc: "Message from andi",
+    link: "/matricula"
+  },
+  {
+    icon: Person,
+    desc: "Estudiantes",
+    secondDesc: "",
+    badge: 0,
+    subList: [],
+    link: "/estudiantes"
+  },
+  {
+    icon: Group,
+    desc: "Grupos",
+    secondDesc: "",
+    badge: 0,
+    subList: [],
+    link: "/grupos"
+  },
+  
+];
+
+const backLogin = [
+  {
+    icon: CalendarMonth,
+    desc: "Calendario",
+    secondDesc: "",
+    subList: [],
+    link: "/calendario"
+  },
+  {
+    icon: FolderOpen,
+    desc: "Matricula",
+    secondDesc: "Message from andi",
+    link: "/matricula"
+  },
+  {
+    icon: Person,
+    desc: "Estudiantes",
+    secondDesc: "",
+    badge: 0,
+    subList: [],
+    link: "/estudiantes"
+  },
+  {
+    icon: Group,
+    desc: "Grupos",
+    secondDesc: "",
+    badge: 0,
+    subList: [],
+    link: "/grupos"
+  },
+  
+];
+
+export default navBarList
+
